b8f545223b4142efea876a7211a33c15fbbb7079
Tom <83533282+wraalabs@users.noreply.github.com>
2021-05-14 16:33:48 +0100

    Update effect_looper.cpp

--- effect_looper.cpp
+++ effect_looper.cpp
@@ -1,9 +1,10 @@
 /*
 * Lofi looper effect that stretches the Teensy's internal RAM by recording at 8-bit & 22050 hz
-* You can get about 10 seconds recorded audio this way
+* You can get about 10 seconds recorded audio this way, run the output through LPFs for less noise
 * "setoctave" plays back at different speeds, pass 0 to this function for half speed, 1 for normal speed and 2 for double speed
 * Pass 1s and 0s to "changeState" whenever you want to start/stop recording and playing
 * Initialise the effect by passing an array of bytes to "begin"
+* I use "isbufferfull" to jump from rec'ing to playing if the record buffer is full. this is done in the sketch
 *
 * Typical open source license, do what ye want with it, idc :D
 * Tom @ Wraa, 2021
@@ -53,22 +54,22 @@ bool AudioEffectLooper::isBufferFull()
 void AudioEffectLooper::setoctave (byte oct)
 {
   octave = oct;
-  use_sample = 1;                                                                     // should move this to update
+  use_sample = 1;                                                             // should move this to update
 }
 
 
 
 void AudioEffectLooper::update(void) {
   
-  audio_block_t *blockw, *block1;                                           // create audio memory blocks and pointers...
+  audio_block_t *blockw, *block1;                                             // create audio memory blocks and pointers...
 
-  int16_t *blockw_pointer;                                                  // can we use only 1 of these? 
+  int16_t *blockw_pointer;                                                    // can we use only 1 of these? 
   int16_t *block_pointer1;
   
 // check if have a delay line
   if (sample_delay_line == NULL) return;
 
-  blockw = receiveWritable();                                               // recieve writable block of samples 
+  blockw = receiveWritable();                                                 // recieve writable block of samples 
   block1 = allocate();                                                        // allocate block for playing samples
   
 
@@ -96,8 +97,6 @@ void AudioEffectLooper::update(void) {
         blockw_pointer++;
       }
     }
-  
-      //transmit(block, 0);                                                   // transmit the block out on channel 0
     release(blockw);
   }
 
@@ -118,7 +117,7 @@ void AudioEffectLooper::update(void) {
             sample = sample_delay_line[read_index++];
             *block_pointer1 = (sample << 8) - 32768;                          // read sample out (no writing on channel 1)
                
-            if (read_index >= loop_end) read_index = 0;                         // restart loop
+            if (read_index >= loop_end) read_index = 0;                       // restart loop
           }
         
         else  *block_pointer1 = sample_delay_line[read_index];                // repeat last read sample (actually the next here)
