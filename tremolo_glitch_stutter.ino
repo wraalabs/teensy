374847b4f623fc37128c3048a91a40e712c7647c
Glowfly <83533282+wraalabs@users.noreply.github.com>
2023-11-09 11:04:32 +0000

    Update tremolo_glitch_stutter.ino

--- tremolo_glitch_stutter.ino
+++ tremolo_glitch_stutter.ino
@@ -5,8 +5,6 @@
 #include "stutterdelay.h"
 
 
-
-// GUI CODE
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -15,6 +13,8 @@
 
 // GUItool: begin automatically generated code
 AudioInputI2S            i2s1;           //xy=208.88333129882812,745.8833312988281
+AudioMixer4              mixer8; //xy=285.8833312988281,1121.4833984375
+AudioMixer4              mixer7; //xy=359.88336181640625,990.4833374023438
 AudioSynthWaveform       waveform1; //xy=466.88330078125,712.9833984375
 AudioFilterBiquad        biquad1; //xy=481.88330078125,509.9833984375
 AudioEffectStutterDelay         delay1;         //xy=485,902
@@ -46,45 +46,48 @@ AudioMixer4              mixer5; //xy=1183.75,908.75
 AudioOutputI2S           i2s2;           //xy=1347.88330078125,853.88330078125
 AudioConnection          patchCord1(i2s1, 0, biquad1, 0);
 AudioConnection          patchCord2(i2s1, 0, multiply1, 0);
-AudioConnection          patchCord3(i2s1, 0, delay1, 0);
-AudioConnection          patchCord4(i2s1, 1, delay2, 0);
-AudioConnection          patchCord5(i2s1, 1, multiply2, 0);
-AudioConnection          patchCord6(i2s1, 1, biquad3, 0);
-AudioConnection          patchCord7(waveform1, biquad2);
-AudioConnection          patchCord8(biquad1, notefreq1);
-AudioConnection          patchCord9(delay1, 0, fade1, 0);
-AudioConnection          patchCord10(delay1, 1, fade2, 0);
-AudioConnection          patchCord11(delay1, 2, fade3, 0);
-AudioConnection          patchCord12(delay1, 3, fade4, 0);
-AudioConnection          patchCord13(delay2, 0, fade5, 0);
-AudioConnection          patchCord14(delay2, 1, fade6, 0);
-AudioConnection          patchCord15(delay2, 2, fade7, 0);
-AudioConnection          patchCord16(delay2, 3, fade8, 0);
-AudioConnection          patchCord17(waveform2, biquad4);
-AudioConnection          patchCord18(biquad3, notefreq2);
-AudioConnection          patchCord19(dc1, 0, mixer1, 0);
-AudioConnection          patchCord20(biquad2, 0, mixer1, 3);
-AudioConnection          patchCord21(dc2, 0, mixer4, 0);
-AudioConnection          patchCord22(multiply1, 0, mixer2, 0);
-AudioConnection          patchCord23(fade1, 0, mixer3, 0);
-AudioConnection          patchCord24(fade3, 0, mixer3, 2);
-AudioConnection          patchCord25(fade5, 0, mixer6, 0);
-AudioConnection          patchCord26(fade2, 0, mixer3, 1);
-AudioConnection          patchCord27(fade4, 0, mixer3, 3);
-AudioConnection          patchCord28(fade7, 0, mixer6, 2);
-AudioConnection          patchCord29(fade6, 0, mixer6, 1);
-AudioConnection          patchCord30(fade8, 0, mixer6, 3);
-AudioConnection          patchCord31(biquad4, 0, mixer4, 3);
-AudioConnection          patchCord32(multiply2, 0, mixer5, 0);
-AudioConnection          patchCord33(mixer1, 0, multiply1, 1);
-AudioConnection          patchCord34(mixer4, 0, multiply2, 1);
-AudioConnection          patchCord35(mixer3, 0, mixer2, 1);
-AudioConnection          patchCord36(mixer6, 0, mixer5, 1);
-AudioConnection          patchCord37(mixer2, 0, i2s2, 0);
-AudioConnection          patchCord38(mixer5, 0, i2s2, 1);
+AudioConnection          patchCord3(i2s1, 0, mixer7, 0);
+AudioConnection          patchCord4(i2s1, 1, multiply2, 0);
+AudioConnection          patchCord5(i2s1, 1, biquad3, 0);
+AudioConnection          patchCord6(i2s1, 1, mixer8, 0);
+AudioConnection          patchCord7(mixer8, delay2);
+AudioConnection          patchCord8(mixer7, delay1);
+AudioConnection          patchCord9(waveform1, biquad2);
+AudioConnection          patchCord10(biquad1, notefreq1);
+AudioConnection          patchCord11(delay1, 0, fade1, 0);
+AudioConnection          patchCord12(delay1, 1, fade2, 0);
+AudioConnection          patchCord13(delay1, 2, fade3, 0);
+AudioConnection          patchCord14(delay1, 3, fade4, 0);
+AudioConnection          patchCord15(delay2, 0, fade5, 0);
+AudioConnection          patchCord16(delay2, 1, fade6, 0);
+AudioConnection          patchCord17(delay2, 2, fade7, 0);
+AudioConnection          patchCord18(delay2, 3, fade8, 0);
+AudioConnection          patchCord19(waveform2, biquad4);
+AudioConnection          patchCord20(biquad3, notefreq2);
+AudioConnection          patchCord21(dc1, 0, mixer1, 0);
+AudioConnection          patchCord22(biquad2, 0, mixer1, 3);
+AudioConnection          patchCord23(dc2, 0, mixer4, 0);
+AudioConnection          patchCord24(multiply1, 0, mixer2, 0);
+AudioConnection          patchCord25(fade1, 0, mixer3, 0);
+AudioConnection          patchCord26(fade3, 0, mixer3, 2);
+AudioConnection          patchCord27(fade5, 0, mixer6, 0);
+AudioConnection          patchCord28(fade2, 0, mixer3, 1);
+AudioConnection          patchCord29(fade4, 0, mixer3, 3);
+AudioConnection          patchCord30(fade7, 0, mixer6, 2);
+AudioConnection          patchCord31(fade6, 0, mixer6, 1);
+AudioConnection          patchCord32(fade8, 0, mixer6, 3);
+AudioConnection          patchCord33(biquad4, 0, mixer4, 3);
+AudioConnection          patchCord34(multiply2, 0, mixer5, 0);
+AudioConnection          patchCord35(mixer1, 0, multiply1, 1);
+AudioConnection          patchCord36(mixer4, 0, multiply2, 1);
+AudioConnection          patchCord37(mixer3, 0, mixer2, 1);
+AudioConnection          patchCord38(mixer3, 0, mixer7, 1);
+AudioConnection          patchCord39(mixer6, 0, mixer5, 1);
+AudioConnection          patchCord40(mixer6, 0, mixer8, 1);
+AudioConnection          patchCord41(mixer2, 0, i2s2, 0);
+AudioConnection          patchCord42(mixer5, 0, i2s2, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=1696.88330078125,124.88330078125
 // GUItool: end automatically generated code
-
 
 
 
@@ -110,7 +113,7 @@ if(!digitalRead(1) && digitalRead(2))   {right = 0; middle = 0; left = 1;}    //
 
 //////////////////// TREMOLO
 float note;
-byte division;
+int division;
 float depth;
 byte waveform = 1;
 byte lastwaveform = 100;
@@ -142,7 +145,7 @@ float floatlag;
 int intlag;
 float mixy;
 int intmixy;
-bool freeze;     // 0 is record so it's called freeze
+bool freeze = 0;     // 0 is record so it's called freeze
 
 
 
@@ -246,7 +249,11 @@ while(left)
    checkToggle();
   
   // Depth
-  depth = (float) analogRead(A3) / 4095;
+  footswitch.update();                                                                                   
+  if(!digitalRead(0)) {depth = 0;}
+  else depth = (float) analogRead(A3) / 4095;
+
+  
   mixer1.gain(0,1-depth);
   mixer1.gain(3,depth);
   mixer4.gain(0,1-depth);
@@ -259,6 +266,7 @@ while(left)
     //division
     division = 7 - ((analogRead(A2) >> 9));// + 2);   // pot controls division, 5 to 2
     division = 2 << division;                         // the 4 divisions are 64 to 8
+   // if
     // set waveform frequency
     note = note / division;
     waveform1.frequency(note);
@@ -308,6 +316,11 @@ if(middle)
     mixer2.gain(1,1);
     mixer5.gain(1,1);
 
+    mixer7.gain(0,1);                     // feedback mixers
+    mixer7.gain(1,0);                     
+    mixer8.gain(0,1);                     
+    mixer8.gain(1,0);                     
+
 
     mixer3.gain(0,1); mixer3.gain(1,1); mixer3.gain(2,1); mixer3.gain(3,1);   // fade mixers
     mixer6.gain(0,1); mixer6.gain(1,1); mixer6.gain(2,1); mixer6.gain(3,1);
@@ -319,6 +332,9 @@ if(middle)
     fade6.fadeOut(1);
     fade7.fadeOut(1);
     fade8.fadeOut(1);
+
+    fadeout();
+    fadein();
 
   while(middle)
   {
@@ -332,7 +348,7 @@ if(middle)
       fadetime = glitchtime / (64 - (analogRead(A1) >>  6)) ; // 64 to  1       // MAKE FLOAT !
       
       density = analogRead(A2);
-      delaytime = (analogRead(A3) / 10) + 100;
+      delaytime = (analogRead(A3) / 11) + 100;
       if(density >= random(4095))
       {
         fadeout();
@@ -352,8 +368,8 @@ if(middle)
   
     footswitch.update();
                                                                                         
-   //   if(!digitalRead(0)) {mixer3.gain(0,0); mixer3.gain(1,1); digitalWrite(LED,HIGH);}                                    // turn on feedback
-   //   else{mixer3.gain(0,1); mixer3.gain(1,0); digitalWrite(LED,LOW);}    
+      if(!digitalRead(0)) {mixer7.gain(0,0); mixer7.gain(1,1); mixer8.gain(0,0); mixer8.gain(1,1); digitalWrite(LEDD,HIGH);}                                    // turn on feedback
+      else{mixer7.gain(0,1); mixer7.gain(1,0); mixer8.gain(0,1); mixer8.gain(1,0); digitalWrite(LEDD,LOW);}    
 }        
 }
 
@@ -377,6 +393,11 @@ if(middle)
     mixer5.gain(0,0);
     mixer2.gain(1,1);
     mixer5.gain(1,1);
+   
+    mixer7.gain(0,1);                     // feedback mixers
+    mixer7.gain(1,0);                     
+    mixer8.gain(0,1);                     
+    mixer8.gain(1,0);    
 
     mixer3.gain(0,1); mixer3.gain(1,1); mixer3.gain(2,0); mixer3.gain(3,0);   // fade mixers
     mixer6.gain(0,1); mixer6.gain(1,1); mixer6.gain(2,0); mixer6.gain(3,0);
@@ -385,11 +406,13 @@ if(middle)
     while(right)
     {
         checkToggle();
-        rate = (8190 - (analogRead(A0) * 2)) + 800 ;
+        rate = (8190 - (analogRead(A0) * 2)) + 1800 ;
         delay1.setrate(rate);
         delay2.setrate(rate);
         
-        cycles = (analogRead(A1) >> 8) + 1;
+        cycles = (analogRead(A1) >> 9) + 1;
+
+        cycles *= 2;
 
         //cycles = 16;
         
@@ -397,27 +420,23 @@ if(middle)
         delay2.setcycles(cycles);
 
 
- // works for every option (too many tho)
- #ifdef worrks     
-   //     intlag = (float) (analogRead(A2) ) ;     // lag pot 
-   //     floatlag = (float) intlag / 4095;        
-   //     floatlag = (float) 1 - floatlag; 
-   //     if(floatlag > 0.97) floatlag = 1;       
-   //     lag = (cycles - 1) * floatlag  ;
-#endif
+
 
         intlag = (float) (analogRead(A2) ) ;     // lag pot 
         floatlag = (float) intlag / 4095;        
         floatlag = (float) 1 - floatlag;
-        if(floatlag > 0.97) floatlag = 1;       
-        lag = ((cycles / 2) - 1) * floatlag  ;
+        if(floatlag > 0.97) floatlag = 1;    
+
+        if(cycles == 2) lag = 1;
+        else    
+          lag = ((cycles / 2) - 1) * floatlag  ;
 
         
 
         //lag = 7;
-         Serial.print(lag);
-         Serial.print('\n');
-        delay(500);
+    //     Serial.print(lag);
+    //     Serial.print('\n');
+    //    delay(500);
         
         delay1.setlag(lag);
         delay2.setlag(lag);
@@ -453,16 +472,14 @@ if(middle)
         if(!digitalRead(0)) freeze = 1;
         else freeze = 0;
 
-        freeze = 0;
         
         delay1.record(freeze);
         delay2.record(freeze);
 
 
-   //     Serial.print(mixy);
-   //      Serial.print('\n');
-
-   //      delay(500);
+     //   Serial.print(freeze);
+    //    Serial.print('\n');
+    //    delay(500);
 
 
         if(delay1.ledstate()) digitalWrite(LEDD,HIGH);
@@ -481,18 +498,22 @@ if(middle)
 
 
 
-// Glitch delay fades
+
+
+
+
+// Glitch fades
 void fadeout ()
 {
-  if(whichfade == 0) {fade1.fadeOut(fadetime); delay1.setdelay(2, delaytime * .29); }
-  if(whichfade == 1) {fade2.fadeOut(fadetime); delay1.setdelay(3, delaytime * .53); }   
-  if(whichfade == 2) {fade3.fadeOut(fadetime); delay1.setdelay(0, delaytime * .74); }
-  if(whichfade == 3) {fade4.fadeOut(fadetime); delay1.setdelay(1, delaytime * 1);   }
+  if(whichfade == 0) {fade1.fadeOut(fadetime); delay1.setdelay(2, delaytime * .29);   fade5.fadeOut(fadetime); delay2.setdelay(2, delaytime * .29); }
+  if(whichfade == 1) {fade2.fadeOut(fadetime); delay1.setdelay(3, delaytime * .53);   fade6.fadeOut(fadetime); delay2.setdelay(3, delaytime * .53); }   
+  if(whichfade == 2) {fade3.fadeOut(fadetime); delay1.setdelay(0, delaytime * .74);   fade7.fadeOut(fadetime); delay2.setdelay(0, delaytime * .74); }
+  if(whichfade == 3) {fade4.fadeOut(fadetime); delay1.setdelay(1, delaytime * 1);     fade8.fadeOut(fadetime); delay2.setdelay(1, delaytime * 1);   }
 }
 void fadein ()
 {
-  if(whichfade == 0) {fade1.fadeIn(fadetime); }
-  if(whichfade == 1) {fade2.fadeIn(fadetime); }
-  if(whichfade == 2) {fade3.fadeIn(fadetime); }
-  if(whichfade == 3) {fade4.fadeIn(fadetime); }
+  if(whichfade == 0) {fade1.fadeIn(fadetime); fade5.fadeIn(fadetime); }
+  if(whichfade == 1) {fade2.fadeIn(fadetime); fade6.fadeIn(fadetime); }
+  if(whichfade == 2) {fade3.fadeIn(fadetime); fade7.fadeIn(fadetime); }
+  if(whichfade == 3) {fade4.fadeIn(fadetime); fade8.fadeIn(fadetime); }
 }
